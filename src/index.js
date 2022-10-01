24887b711ac5623261cdbe889abac8892642f01c
joseSoberon <jose.ant.soberon@gmail.com>
2022-10-01 12:41:03 -0500

    Create classes for all inputs and add logic for the form validation

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import { Input } from './inputElement';
+import { Input, Select, Password } from './inputElement';
 
-let email = Input("email", "email-error");
-email.addEvent("focusout");
+let mail = new Input("email", "email-error");
+mail.addEvent("focusout");
 
-let country = Input("country", "country-error")
-country.checkSelection();
+let country = new Select("country", "country-error");
+country.addEvent("input");
 
-let zipCode = Input("zip-code", "zip-error");
+let zipCode = new Input("zip-code", "zip-error");
 zipCode.addEvent("focusout");
+
+let password = new Password("password", "password-error", "confirm-password", "confirm-error")
+password.addEvent("focusout");
+password.confirmPassword();
--- src/inputElement.js
+++ src/inputElement.js
@@ -1,3 +1,4 @@
+/*
 const Input = (input, inputError) => {
     let element = document.getElementById(input);
     let error = document.getElementById(inputError);
@@ -42,6 +43,13 @@ const Input = (input, inputError) => {
         })
     }
 
+    function checkPassword(password) {
+        element.addEventListener("focusout", () => {
+            console.log(password);
+            console.log(element.value);
+        })
+    }
+
     function setValidityClass(validityClass, lastClass) {
         if (!element.classList.contains(validityClass)) {
             if (element.classList.contains(lastClass)) {
@@ -52,7 +60,97 @@ const Input = (input, inputError) => {
     }
 
 
-    return { element, error, addEvent, checkSelection };
+    return { element, error, addEvent, checkSelection, checkPassword };
 };
+*/
 
-export { Input };
+class Element {
+    constructor(id, idError) {
+        this.container = document.getElementById(id);
+        this.error = document.getElementById(idError);
+    }
+
+    setValidityClass(validityClass, lastClass) {
+        if (!this.container.classList.contains(validityClass)) {
+            if (this.container.classList.contains(lastClass)) {
+                this.container.classList.remove(lastClass);
+            }
+            this.container.classList.add(validityClass);
+        }
+    }
+}
+
+class Input extends Element {
+    constructor(id, idError) {
+        super(id, idError);
+    }
+
+    addEvent(action) {
+        this.container.addEventListener(action, () => {
+            if (this.container.validity.valid) {
+                this.error.textContent = "";
+                this.error.className = "error";
+                this.setValidityClass("valid", "invalid");
+            }
+            else {
+                this.checkMissing();
+                this.checkMismatch();
+                this.setValidityClass("invalid", "valid");
+            }
+        })
+    }
+
+    checkMissing() {
+        if (this.container.validity.valueMissing) {
+            this.error.textContent = "Please complete this field";
+        }
+    }
+
+    checkMismatch() {
+        if (this.container.validity.typeMismatch || this.container.validity.patternMismatch) {
+            console.log("Hello")
+            this.error.textContent = "Entered value need to be a valid format";
+        }
+    }
+}
+
+class Select extends Element {
+    constructor(id, idError) {
+        super(id, idError);
+    }
+
+    addEvent(action) {
+        this.container.addEventListener(action, () => {
+            if(this.container.value != "default") {
+                this.error.textContent = "";
+                this.error.className = "error";
+                this.setValidityClass("valid", "invalid");
+            }
+            else {
+                this.error.textContent = "Please Select a Country";
+            }
+        })
+    }
+}
+
+class Password extends Input {
+    constructor(id, idError, idConfirm, idConfirmError) {
+        super(id, idError);
+        this.confirmContainer = document.getElementById(idConfirm);
+        this.confirmError = document.getElementById(idConfirmError)
+    }
+
+    confirmPassword() {
+        this.confirmContainer.addEventListener("input", () => {
+            if (this.container.value == this.confirmContainer.value) {
+                this.confirmError.textContent = "";
+                this.confirmError.className = "error";
+            }
+            else {
+                this.confirmError.textContent = "Your password doesn't match";
+            }
+        })
+    }
+}
+
+export { Input, Select, Password };
